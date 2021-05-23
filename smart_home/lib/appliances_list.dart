3020dc0c8f5eea947aee34d8a192bb80bc556378
Ulik Abdykerimov <inf3293@hs-worms.de>
2021-05-23 08:14:46 +0200

    TextFields added

--- smart_home/lib/appliances_list.dart
+++ smart_home/lib/appliances_list.dart
@@ -1,33 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AppliancesList extends StatefulWidget {
   @override
-  State createState() => new AppliancesListState();
+  State createState() => AppliancesListState();
 }
 
 class AppliancesListState extends State<AppliancesList> {
   List<String> items = [];
-  final TextEditingController nCtxt = new TextEditingController();
+  final microwave = TextEditingController();
+  final coffeeMaker = TextEditingController();
+  final mixer = TextEditingController();
+
+  String textFieldsAreEmpty() {
+    if (microwave.text.isEmpty) {
+      return "Microwave";
+    }
+    if (coffeeMaker.text.isEmpty) {
+      return "Coffee Maker";
+    }
+    if (mixer.text.isEmpty) {
+      return "Mixer";
+    }
+    return "";
+  }
+
   @override
   Widget build(BuildContext ctxt) {
-    return new Scaffold(
-        body: new Column(
-      children: <Widget>[
-        new TextField(
-          controller: nCtxt,
-          onSubmitted: (text) {
-            items.add(text);
-            nCtxt.clear();
-            setState(() {});
-          },
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Microwave'),
+              controller: microwave,
+              onSubmitted: (text) {
+                items.add(text);
+                microwave.clear();
+                setState(() {});
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Coffee Maker'),
+              controller: coffeeMaker,
+              onSubmitted: (text) {
+                items.add(text);
+                coffeeMaker.clear();
+                setState(() {});
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Mixer'),
+              controller: mixer,
+              onSubmitted: (text) {
+                items.add(text);
+                mixer.clear();
+                setState(() {});
+              },
+            ),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (BuildContext ctxt, int Index) {
+                      return Text(items[Index]);
+                    }))
+          ],
         ),
-        new Expanded(
-            child: new ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (BuildContext ctxt, int Index) {
-                  return new Text(items[Index]);
-                }))
-      ],
-    ));
+      ),
+    );
   }
 }
